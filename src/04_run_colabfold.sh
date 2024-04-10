24bcb630e0058fe606dd201a3956eb18752f42d9
thomas-tams-dtu <s204540@dtu.dk>
2024-04-10 19:56:16 +0200

    build: script for running LSF colabfold scripts

--- src/04_run_colabfold.sh
+++ src/04_run_colabfold.sh
@@ -5,11 +5,11 @@ complexes_dir=data/complexes
 # Loop through each UniProt ID in the file
 while IFS= read -r complex; do
     # Write LSF jobscript from template by replacing <complex> with the current complex
-    cat "src/colabfold_jobscript_template.sh" | sed "s/<complex>/${complex}/g" > "${complexes_dir}/${complex}_jobscript.sh"
+    cat "src/colabfold_jobscript_template.sh" | sed "s/<complex>/${complex}/g" > "src/${complex}_jobscript.sh"
 
     # Run jobscript
-    bsub < "${complexes_dir}/${complex}_jobscript.sh"
+    bsub < "src/${complex}_jobscript.sh"
 
     # Remove jobscript
-    # rm "${complexes_dir}/${complex}_jobscript.sh"
+    # rm "src/${complex}_jobscript.sh"
 done < "${complexes_dir}/complex_fastas.txt"
