212423ccb69556f6e71657300038c3cdfb208856
thomas-tams-dtu <s204540@dtu.dk>
2024-04-11 22:46:09 +0200

    fix: change from canonical names to uniprot ids

--- src/run_pdb_miner.sh
+++ src/run_pdb_miner.sh
@@ -25,16 +25,18 @@ cut -d ',' -f 2 $csv_file | tail -n +2 | tail -n 1 > ${pdb_miner_dir}/tmp_names.
 cut -d ',' -f 4 $csv_file | tail -n +2 >> ${pdb_miner_dir}/tmp_names.txt               # Grabbing other proteins of interaction network
 
 
-readarray -t names < ${pdb_miner_dir}/tmp_names.txt
-readarray -t ids < ${pdb_miner_dir}/tmp_ids.txt
+readarray -t names < ${pdb_miner_dir}/names.txt
+readarray -t ids < ${pdb_miner_dir}/ids.txt
 
 # Read each name from the list and create a directory for it
-for ((i = 0; i < ${#names[@]}; i++)); do
-    mkdir ${pdb_miner_dir}/${names[i]}
-    cd ${pdb_miner_dir}/${names[i]}
+for ((i = 0; i < ${#ids[@]}; i++)); do
+    mkdir ${pdb_miner_dir}/${ids[i]}
+    cd ${pdb_miner_dir}/${ids[i]}
     echo "/home/ctools/PDBminer/PDBminer -u ${ids[i]} -n 4 -f csv"
     /home/ctools/PDBminer/PDBminer -u ${ids[i]} -n 4 -f csv
-    cd ..
+    
+    # Go back to the parent directory
+    cd ..   
     cd ..
     cd ..
 done
